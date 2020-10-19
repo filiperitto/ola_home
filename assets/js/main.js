059f6d6f75264fb1ea8bd007821f7b790ab931a8
Filipe Ritto <filipe.ritto@gmail.com>
2020-10-19 14:29:18 -0300

    Firt update

---
+++ assets/js/main.js
@@ -0,0 +1,11 @@
+if ($(window).width() > 750) {
+    $(".scrollhere").niceScroll({
+        cursorcolor: "#ccc",
+        preventmultitouchscrolling: true
+    });  
+
+    $(".scrollola").niceScroll({
+        cursorcolor: "#ccc",
+        preventmultitouchscrolling: true
+    }); 
+ }
