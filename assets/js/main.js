745a4b05a1e8919d1fa51180f063d81adad22611
Filipe Ritto <filipe.ritto@gmail.com>
2020-11-04 17:51:42 -0300

    0.0.8
    fix responsible list canais

--- assets/js/main.js
+++ assets/js/main.js
@@ -41,7 +41,7 @@ $(function(){
  
 });
 
-$(document).ready(function () {
+$(function () {
 
     /*=== light-mode Switch ===*/
     $('.light-mode-cta').click(function () {
@@ -109,6 +109,10 @@ $(document).ready(function () {
         $('.epi-playlist').removeClass('select');
         $('.queue-playlist').addClass('select');
     });
+
+
+
+});
     
     /*=== Interação do menu ===
     $('.cta-menu-epi').click(function () {
@@ -120,7 +124,7 @@ $(document).ready(function () {
 
 
     $('.originais-carousel').owlCarousel({
-        loop: true,
+        loop: false,
         autoWidth: true,
         //margin: 10,
         nav: true,
@@ -143,75 +147,47 @@ $(document).ready(function () {
                 items: 7
             },
             750: {
-                items: 3
+                items: 3,
+                loop: true,
             },
             940: {
-                items: 4
+                items: 4,
+                loop: true,
             },
             1100: {
-                items: 5
+                items: 5,
+                loop: true,
             },
             1300: {
-                items: 6
+                items: 6,
+                loop: true,
             },
             1500: {
-                items: 7
+                items: 7,
+                loop: true,
             },
             1600: {
-                items: 8
+                items: 8,
+                loop: true,
             },
             1780: {
-                items: 9
+                items: 9,
+                loop: true,
             }
         }
     })
 
     $('.epi-carousel').owlCarousel({
-        loop: true,
         //margin: 10,
+        loop: false,
         nav: true,
         navText: ["<div class='nav-btn prev-slide'><svg width='8' height='14' viewBox='0 0 8 14' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M2.58348 6.78788L7.71598 11.9204C8.09467 12.2991 8.09467 12.9131 7.71598 13.2917C7.33729 13.6704 6.72332 13.6704 6.34462 13.2917L0.526443 7.47356C0.147752 7.09487 0.147752 6.48089 0.526443 6.1022L6.34462 0.284018C6.72331 -0.0946724 7.33729 -0.0946725 7.71598 0.284018C8.09467 0.662708 8.09467 1.27669 7.71598 1.65538L2.58348 6.78788Z' fill='#F2F2F2'/></svg>    </div>", "<div class='nav-btn next-slide'><svg width='8' height='14' viewBox='0 0 8 14' fill='none' ><path d='M5.41652 7.21212L0.284018 2.07962C-0.0946726 1.70093 -0.0946726 1.08695 0.284018 0.708259C0.662708 0.329569 1.27669 0.329569 1.65538 0.708259L7.47356 6.52644C7.85225 6.90513 7.85225 7.51911 7.47356 7.8978L1.65538 13.716C1.27669 14.0947 0.662708 14.0947 0.284018 13.716C-0.0946726 13.3373 -0.0946726 12.7233 0.284018 12.3446L5.41652 7.21212Z' fill='#F2F2F2'/></svg></div>"],
         autoWidth: true,
         responsive: {
-            0: {
-                items: 2
-            },
-            370: {
-                items: 3
-            },
-            528: {
-                items: 5
-            },
-            614: {
-                items: 6
-            },
-            718: {
-                items: 7
-            },
             750: {
-                items: 3
+                //items: 3,
+                loop: true
             },
-            800: {
-                items: 4
-            },
-            910: {
-                items: 5
-            },
-            1100: {
-                items: 6
-            },
-            1200: {
-                items: 7
-            },
-            1400: {
-                items: 8
-            },
-            1550: {
-                items: 9
-            },
-            1650: {
-                items: 10
-            }
         }
     })
     
@@ -243,10 +219,6 @@ $(document).ready(function () {
             }
         }
     })
-
-
-
-});
 
 /*
   var swiper = new Swiper('.swiper-container', {
