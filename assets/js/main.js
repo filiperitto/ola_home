b805da660472bd4dff6b0cf87ce31ac116d76610
Filipe Ritto <filipe.ritto@gmail.com>
2020-11-04 20:40:11 -0300

    0.1.0
    Upgrade do Slider puro

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,16 +5,18 @@ $(function(){
         trigger: 'left',
         items: $.contextMenu.fromMenu($('#contextMenu-episode')),        
     });
-    $('.context-menu-list').css({ top: y + 50, left: x })
+    $('.context-menu-list').css({ top: y + 50, left: x });
 });
 
 $(function(){
     $.contextMenu({
         selector: '.cta-share', 
         trigger: 'left',
-        items: $.contextMenu.fromMenu($('#contextMenu-share')),        
+        items: $.contextMenu.fromMenu($('#contextMenu-share')),  
+        top: y + 50, 
+        left: x       
     });
-    $('.context-menu-list').css({ top: y + 50, left: x })
+    $('.context-menu-list').css({ top: y + 50, left: x });
 });
 
  $(document).ready(function() {
@@ -113,113 +115,96 @@ $(function () {
 
 
 });
-    
-    /*=== Interação do menu ===
-    $('.cta-menu-epi').click(function () {
-        $(this).toggleClass('open');
+
+    new Glider(document.querySelector('.epi-glider'), {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: {
+          prev: '.prev-epi',
+          next: '.next-epi'
+        },
+        responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 'auto',
+              slidesToScroll: 1,
+              itemWidth: 150
+            }
+          }
+        ]
+      });
+
+    new Glider(document.querySelector('.originais-glider'), {
+        slidesToShow: 'auto',
+        slidesToScroll: 1,
+        arrows: {
+        prev: '.prev-ori',
+        next: '.next-ori'
+        },
+        responsive: [
+        {
+            breakpoint: 350,
+            settings: {
+            itemWidth: 107
+            }
+        },
+        {
+            breakpoint: 425,
+            settings: {
+            itemWidth: 129
+            }
+        },
+        {
+            breakpoint: 450,
+            settings: {
+            itemWidth: 101
+            }
+        },
+        {
+            breakpoint: 750,
+            settings: {
+            itemWidth: 166
+            }
+        }
+        ]
     });
-    $('.inv-modal-menu').click(function () {
-        $(this).removeClass('open');
-    });*/
 
-
-    $('.originais-carousel').owlCarousel({
-        loop: false,
-        autoWidth: true,
-        //margin: 10,
-        nav: true,
-        navText: ["<div class='nav-btn prev-slide'><svg width='8' height='14' viewBox='0 0 8 14' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M2.58348 6.78788L7.71598 11.9204C8.09467 12.2991 8.09467 12.9131 7.71598 13.2917C7.33729 13.6704 6.72332 13.6704 6.34462 13.2917L0.526443 7.47356C0.147752 7.09487 0.147752 6.48089 0.526443 6.1022L6.34462 0.284018C6.72331 -0.0946724 7.33729 -0.0946725 7.71598 0.284018C8.09467 0.662708 8.09467 1.27669 7.71598 1.65538L2.58348 6.78788Z' fill='#F2F2F2'/></svg>    </div>", "<div class='nav-btn next-slide'><svg width='8' height='14' viewBox='0 0 8 14' fill='none' ><path d='M5.41652 7.21212L0.284018 2.07962C-0.0946726 1.70093 -0.0946726 1.08695 0.284018 0.708259C0.662708 0.329569 1.27669 0.329569 1.65538 0.708259L7.47356 6.52644C7.85225 6.90513 7.85225 7.51911 7.47356 7.8978L1.65538 13.716C1.27669 14.0947 0.662708 14.0947 0.284018 13.716C-0.0946726 13.3373 -0.0946726 12.7233 0.284018 12.3446L5.41652 7.21212Z' fill='#F2F2F2'/></svg></div>"],
-        stagePadding: 0,
-        responsive: {
-            0: {
-                items: 3
-            },
-            450: {
-                items: 4
-            },
-            528: {
-                items: 5
-            },
-            614: {
-                items: 6
-            },
-            718: {
-                items: 7
-            },
-            750: {
-                items: 3,
-                loop: true,
-            },
-            940: {
-                items: 4,
-                loop: true,
-            },
-            1100: {
-                items: 5,
-                loop: true,
-            },
-            1300: {
-                items: 6,
-                loop: true,
-            },
-            1500: {
-                items: 7,
-                loop: true,
-            },
-            1600: {
-                items: 8,
-                loop: true,
-            },
-            1780: {
-                items: 9,
-                loop: true,
+    new Glider(document.querySelector('.genero-glider'), {
+        slidesToShow: 2.5,
+        slidesToScroll: 1,
+        arrows: {
+          prev: '.prev-gen',
+          next: '.next-gen'
+        },
+        responsive: [
+          {
+            breakpoint: 750,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 3,
+              slidesToScroll: 1
             }
-        }
-    })
-
-    $('.epi-carousel').owlCarousel({
-        //margin: 10,
-        loop: false,
-        nav: true,
-        navText: ["<div class='nav-btn prev-slide'><svg width='8' height='14' viewBox='0 0 8 14' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M2.58348 6.78788L7.71598 11.9204C8.09467 12.2991 8.09467 12.9131 7.71598 13.2917C7.33729 13.6704 6.72332 13.6704 6.34462 13.2917L0.526443 7.47356C0.147752 7.09487 0.147752 6.48089 0.526443 6.1022L6.34462 0.284018C6.72331 -0.0946724 7.33729 -0.0946725 7.71598 0.284018C8.09467 0.662708 8.09467 1.27669 7.71598 1.65538L2.58348 6.78788Z' fill='#F2F2F2'/></svg>    </div>", "<div class='nav-btn next-slide'><svg width='8' height='14' viewBox='0 0 8 14' fill='none' ><path d='M5.41652 7.21212L0.284018 2.07962C-0.0946726 1.70093 -0.0946726 1.08695 0.284018 0.708259C0.662708 0.329569 1.27669 0.329569 1.65538 0.708259L7.47356 6.52644C7.85225 6.90513 7.85225 7.51911 7.47356 7.8978L1.65538 13.716C1.27669 14.0947 0.662708 14.0947 0.284018 13.716C-0.0946726 13.3373 -0.0946726 12.7233 0.284018 12.3446L5.41652 7.21212Z' fill='#F2F2F2'/></svg></div>"],
-        autoWidth: true,
-        responsive: {
-            750: {
-                //items: 3,
-                loop: true
-            },
-        }
-    })
-    
-    $('.genero-carousel').owlCarousel({
-        //loop: true,
-        //margin: 10,
-        nav: true,
-        navText: ["<div class='nav-btn prev-slide'><svg width='8' height='14' viewBox='0 0 8 14' fill='none' xmlns='http://www.w3.org/2000/svg'><path d='M2.58348 6.78788L7.71598 11.9204C8.09467 12.2991 8.09467 12.9131 7.71598 13.2917C7.33729 13.6704 6.72332 13.6704 6.34462 13.2917L0.526443 7.47356C0.147752 7.09487 0.147752 6.48089 0.526443 6.1022L6.34462 0.284018C6.72331 -0.0946724 7.33729 -0.0946725 7.71598 0.284018C8.09467 0.662708 8.09467 1.27669 7.71598 1.65538L2.58348 6.78788Z' fill='#F2F2F2'/></svg>    </div>", "<div class='nav-btn next-slide'><svg width='8' height='14' viewBox='0 0 8 14' fill='none' ><path d='M5.41652 7.21212L0.284018 2.07962C-0.0946726 1.70093 -0.0946726 1.08695 0.284018 0.708259C0.662708 0.329569 1.27669 0.329569 1.65538 0.708259L7.47356 6.52644C7.85225 6.90513 7.85225 7.51911 7.47356 7.8978L1.65538 13.716C1.27669 14.0947 0.662708 14.0947 0.284018 13.716C-0.0946726 13.3373 -0.0946726 12.7233 0.284018 12.3446L5.41652 7.21212Z' fill='#F2F2F2'/></svg></div>"],
-        //autoWidth: true,
-        responsive: {
-            0: {
-                items: 2,
-                autoWidth: true,
-            },
-            500: {
-                items: 3
-            },
-            700: {
-                items: 4
-            },
-            750: {
-                items: 3
-            },
-            1200: {
-                items: 4
-            },
-            1350: {
-                items: 7
+          },
+          {
+            breakpoint: 1200,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 4,
+              slidesToScroll: 1
             }
-        }
-    })
-
+          },
+          {
+            breakpoint: 1350,
+            settings: {
+              // Set to `auto` and provide item width to adjust to viewport
+              slidesToShow: 7,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      });
 /*
   var swiper = new Swiper('.swiper-container', {
     // Default parameters
