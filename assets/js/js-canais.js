b36f24c085688b33501c856f3f573e3915213b82
Filipe Ritto <filipe.ritto@gmail.com>
2020-12-01 21:17:19 -0300

    Update

--- assets/js/js-canais.js
+++ assets/js/js-canais.js
@@ -6,3 +6,51 @@
     document.querySelector('.cta-vermais').addEventListener('click', ver_maisToggle);
 })();
 //------------------------ Toggle ver-mais hero
+
+
+(function(){
+    function viewPlaylistToggle() {
+        document.getElementById("view-square").classList.toggle('active');
+        document.getElementById("view-list").classList.toggle('active');
+        document.getElementById("view-listID").classList.toggle('list-view');
+        
+
+    }
+    // Select all the elements with example class.
+    var ToggleviewPlaylist = document.querySelectorAll('.cta-view');
+    // Loop through the elements.
+    for (var i = 0; i < ToggleviewPlaylist.length; i++) {
+    // Add the class margin to the individual elements.
+    ToggleviewPlaylist[i].addEventListener('click', viewPlaylistToggle);
+    }
+
+
+    
+})();
+//------------------------ Toggle view playlist Canal
+
+
+    
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click();
+
+function openTab(evt, canalSwitch) {
+    // Declare all variables
+    var i, tab_painel, tablinks;
+  
+    // Get all elements with class="tab-painel" and hide them
+    tab_painel = document.getElementsByClassName("tab_painel");
+    for (i = 0; i < tab_painel.length; i++) {
+      tab_painel[i].style.display = "none";
+    }
+  
+    // Get all elements with class="tablinks" and remove the class "active"
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    // Show the current tab, and add an "active" class to the button that opened the tab
+    document.getElementById(canalSwitch).style.display = "block";
+    evt.currentTarget.className += " active";
+  }
