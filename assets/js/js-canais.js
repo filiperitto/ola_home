95374517aef68dbb8f45e456666ea945cde6da27
Filipe Ritto <filipe.ritto@gmail.com>
2020-12-06 23:32:42 -0300

    update

--- assets/js/js-canais.js
+++ assets/js/js-canais.js
@@ -1,3 +1,14 @@
+//== Lightbox About-2
+// https://biati-digital.github.io/glightbox/
+(function(){
+  var lightboxInlineIframe = GLightbox({
+      'selector': '.glightbox4'
+  });
+})();
+//======================== /Lightbox Sobre
+
+
+
 (function(){
     function ver_maisToggle() {
         this.classList.toggle('active');
